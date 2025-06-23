508d4860951c81c01aa5bbba880f74e313646596
divyamaddipati <21b01a12a2@svecw.edu.in>
2025-06-23 21:44:29 +0530

    chaged

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-export const useSocket = (serverUrl: string = 'http://localhost:3001') => {
+export const useSocket = (serverUrl: string = 'https://wboardcast-backend.llp.trizenventures.com') => {
   const socketRef = useRef<Socket | null>(null);
 
   useEffect(() => {
