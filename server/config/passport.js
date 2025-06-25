68bac9f0229245b0541091c388cee5eaa5ac9b8a
Allam GowriSankar <126642378+GowrisankarAllam001@users.noreply.github.com>
2025-06-25 18:29:05 +0530

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -50,7 +50,7 @@ export const initializeOAuthStrategies = async () => {
       passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://wboardcast-backend.llp.trizenventures.com/api/auth/google/callback"
+        callbackURL: "/api/auth/google/callback"
       }, async (accessToken, refreshToken, profile, done) => {
         try {
           let user = await User.findOne({ googleId: profile.id });
